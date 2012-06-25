fc4f7154948246c758334fe1332f4dbabaf62ac9
dtkaczyk <dtkaczyk@bd31ee22-090f-0410-9135-988eccef8701>
2012-06-25 12:26:50 +0000

    hmm zone classification demo added

---
+++ yadda-analysis-impl/src/main/java/pl/edu/icm/yadda/analysis/metadata/zoneclassification/HMMZoneClassificationDemo.java
@@ -0,0 +1,107 @@
+package pl.edu.icm.yadda.analysis.metadata.zoneclassification;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureCalculator;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureVector;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureVectorBuilder;
+import pl.edu.icm.yadda.analysis.classification.features.SimpleFeatureVectorBuilder;
+import pl.edu.icm.yadda.analysis.classification.hmm.HMMService;
+import pl.edu.icm.yadda.analysis.classification.hmm.HMMServiceImpl;
+import pl.edu.icm.yadda.analysis.classification.hmm.probability.HMMProbabilityInfo;
+import pl.edu.icm.yadda.analysis.classification.hmm.probability.HMMProbabilityInfoFactory;
+import pl.edu.icm.yadda.analysis.classification.hmm.training.HMMTrainingElement;
+import pl.edu.icm.yadda.analysis.metadata.zoneclassification.features.*;
+import pl.edu.icm.yadda.analysis.metadata.zoneclassification.nodes.BxDocsToFVHMMTrainingElementsConverterNode;
+import pl.edu.icm.yadda.analysis.textr.HMMZoneClassifier;
+import pl.edu.icm.yadda.analysis.textr.model.BxDocument;
+import pl.edu.icm.yadda.analysis.textr.model.BxPage;
+import pl.edu.icm.yadda.analysis.textr.model.BxZone;
+import pl.edu.icm.yadda.analysis.textr.model.BxZoneLabel;
+import pl.edu.icm.yadda.analysis.textr.transformers.TrueVizToBxDocumentReader;
+import pl.edu.icm.yadda.metadata.transformers.TransformationException;
+
+/**
+ *
+ * @author Dominika Tkaczyk
+ */
+public class HMMZoneClassificationDemo {
+
+    protected static final String hmmTestFile = "/pl/edu/icm/yadda/analysis/metadata/zoneclassification/09629351.xml";
+
+    public static void main(String[] args) throws TransformationException, Exception {
+        
+        // 1. construct vector of features builder
+        FeatureVectorBuilder<BxZone, BxPage> vectorBuilder =
+                new SimpleFeatureVectorBuilder<BxZone, BxPage>();
+        vectorBuilder.setFeatureCalculators(Arrays.<FeatureCalculator<BxZone, BxPage>>asList(
+                new ProportionsFeature(),
+                new HeightFeature(),
+                new WidthFeature(),
+                new XPositionFeature(),
+                new YPositionFeature(),
+                new HeightRelativeFeature(),
+                new WidthRelativeFeature(),
+                new XPositionRelativeFeature(),
+                new YPositionRelativeFeature(),
+                new LineCountFeature(),
+                new LineRelativeCountFeature(),
+                new LineHeightMeanFeature(),
+                new LineWidthMeanFeature(),
+                new LineXPositionMeanFeature(),
+                new LineXPositionDiffFeature(),
+                new LineXWidthPositionDiffFeature(),
+                new WordCountFeature(),
+                new WordCountRelativeFeature(),
+                new CharCountFeature(),
+                new CharCountRelativeFeature(),
+                new DigitCountFeature(),
+                new DigitRelativeCountFeature(),
+                new LetterCountFeature(),
+                new LetterRelativeCountFeature(),
+                new LowercaseCountFeature(),
+                new LowercaseRelativeCountFeature(),
+                new UppercaseCountFeature(),
+                new UppercaseRelativeCountFeature(),
+                new UppercaseWordCountFeature(),
+                new UppercaseWordRelativeCountFeature(),
+                new AtCountFeature(),
+                new AtRelativeCountFeature(),
+                new CommaCountFeature(),
+                new CommaRelativeCountFeature(),
+                new DotCountFeature(),
+                new DotRelativeCountFeature(),
+                new WordWidthMeanFeature()
+                ));
+
+        // 2. import and generate training set based on sequences and vector of features
+        InputStream is = HMMZoneClassificationDemo.class.getResourceAsStream(hmmTestFile);
+        InputStreamReader isr = new InputStreamReader(is);
+        
+        TrueVizToBxDocumentReader reader = new TrueVizToBxDocumentReader();
+        BxDocument document = new BxDocument().setPages(reader.read(isr));
+        
+        BxDocsToFVHMMTrainingElementsConverterNode node = new BxDocsToFVHMMTrainingElementsConverterNode();
+        node.setFeatureVectorBuilder(vectorBuilder);
+        HMMTrainingElement<BxZoneLabel, FeatureVector>[] trainingElements = node.process(new BxDocument[] {document}, null);
+
+        // 3. HMM training. The resulting probabilities object should be serialized for further usage
+        HMMProbabilityInfo<BxZoneLabel, FeatureVector> hmmProbabilities
+                = HMMProbabilityInfoFactory.getFVHMMProbability(trainingElements, vectorBuilder);
+
+
+        // 4. zone classifier instance
+        HMMZoneClassifier zoneClassifier = new HMMZoneClassifier(new HMMServiceImpl(), hmmProbabilities, vectorBuilder);
+
+        // 5. find the most probable labels for HMM objects
+        zoneClassifier.classifyZones(document);
+        
+        for (BxPage page : document.getPages()) {
+            for (BxZone zone : page.getZones()) {
+                System.out.println(zone.getLabel());
+            }
+        }
+    }
+}

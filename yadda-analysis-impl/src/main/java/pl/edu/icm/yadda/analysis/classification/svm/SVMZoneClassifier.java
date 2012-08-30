d32726f18d3366173710a7f9e5fc490b8dfc6137
pszostek <pszostek@bd31ee22-090f-0410-9135-988eccef8701>
2012-08-30 13:17:30 +0000

    Add SVM zone classifier

---
+++ yadda-analysis-impl/src/main/java/pl/edu/icm/yadda/analysis/classification/svm/SVMZoneClassifier.java
@@ -0,0 +1,223 @@
+package pl.edu.icm.yadda.analysis.classification.svm;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import pl.edu.icm.yadda.analysis.AnalysisException;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureVector;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureVectorBuilder;
+import pl.edu.icm.yadda.analysis.classification.features.SimpleFeatureVector;
+import pl.edu.icm.yadda.analysis.classification.hmm.training.HMMTrainingElement;
+import pl.edu.icm.yadda.analysis.textr.ZoneClassifier;
+import pl.edu.icm.yadda.analysis.textr.model.BxDocument;
+import pl.edu.icm.yadda.analysis.textr.model.BxPage;
+import pl.edu.icm.yadda.analysis.textr.model.BxZone;
+import pl.edu.icm.yadda.analysis.textr.model.BxZoneLabel;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
+import net.sf.javaml.core.Instance;
+import libsvm.LibSVM;
+import libsvm.svm_parameter;
+
+public class SVMZoneClassifier implements ZoneClassifier {
+	final static svm_parameter defaultParameter = new svm_parameter();		
+	static {
+		// default values
+		defaultParameter.svm_type = svm_parameter.C_SVC;
+		defaultParameter.C = 2048;
+		defaultParameter.kernel_type = svm_parameter.LINEAR;
+		defaultParameter.degree = 3;
+		defaultParameter.gamma = 128; // 1/k
+		defaultParameter.coef0 = 0.5;
+		defaultParameter.nu = 0.5;
+		defaultParameter.cache_size = 100;
+		defaultParameter.eps = 1e-3;
+		defaultParameter.p = 0.1;
+		defaultParameter.shrinking = 1;
+		defaultParameter.probability = 0;
+		defaultParameter.nr_weight = 0;
+		defaultParameter.weight_label = new int[0];
+		defaultParameter.weight = new double[0];
+	}
+	private FeatureVectorBuilder<BxZone, BxPage> featureVectorBuilder;
+	private final LibSVM svm = new LibSVM();
+	private FeatureLimits[] limits;
+	private String[] features;
+	
+	
+	public SVMZoneClassifier(List<BxZoneLabel> zoneLabels, FeatureVectorBuilder<BxZone, BxPage> featureVectorBuilder) 
+	{
+		//discard zoneLabels;
+		this.featureVectorBuilder = featureVectorBuilder;
+		limits = new FeatureLimits[featureVectorBuilder.size()];
+
+		//set default limits to: max = -inf, min = +inf
+		for(int idx=0; idx<featureVectorBuilder.size(); ++idx) {
+			limits[idx] = new FeatureLimits(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
+		}		
+		
+		svm_parameter param = getDefaultParam();
+		svm.setParameters(param);
+	}
+	
+	public LibSVM getSVM() {
+		return svm;
+	}
+	
+	public svm_parameter getDefaultParam() {
+		svm_parameter param = new svm_parameter();
+		// default values
+		param.svm_type = defaultParameter.svm_type;
+		param.C = defaultParameter.C;
+		param.kernel_type = defaultParameter.kernel_type;
+		param.degree = defaultParameter.degree;
+		param.gamma = defaultParameter.gamma; // 1/k
+		param.coef0 = defaultParameter.coef0;
+		param.nu = defaultParameter.nu;
+		param.cache_size = defaultParameter.cache_size;
+		param.eps = defaultParameter.eps;
+		param.p = defaultParameter.p;
+		param.shrinking = defaultParameter.shrinking;
+		param.probability = defaultParameter.probability;
+		param.nr_weight = defaultParameter.nr_weight;
+		param.weight_label = defaultParameter.weight_label;
+		param.weight = defaultParameter.weight;
+		return param;
+	}
+	
+	private void setFeatureLimits(List<HMMTrainingElement<BxZoneLabel>> trainingElements) 
+	{
+		for(HMMTrainingElement<BxZoneLabel> trainingElem: trainingElements) {
+			FeatureVector fv = trainingElem.getObservation();
+			Set<String> names = fv.getFeatureNames();
+			
+			int featureIdx = 0;
+			for(String name: names) {
+				double val = fv.getFeature(name);
+				if(val > limits[featureIdx].max) {
+					limits[featureIdx].setMax(val);
+				}
+				if(val < limits[featureIdx].min){
+					limits[featureIdx].setMin(val);
+				}
+				++featureIdx;
+			}
+		}
+		for(FeatureLimits limit: limits) {
+			assert limit.getMin() != Double.MAX_VALUE;
+			assert limit.getMax() != Double.MIN_VALUE;
+		}
+	}
+	
+	public void buildClassifier(List<HMMTrainingElement<BxZoneLabel>> trainingElements) 
+	{
+		assert trainingElements.size() > 0;
+		if(features == null) {
+			features = (String[])trainingElements.get(0).getObservation().getFeatureNames().toArray(new String[1]);
+		}
+		setFeatureLimits(trainingElements);
+		Dataset data = buildDatasetForTraining(trainingElements);
+		svm.buildClassifier(data);
+	}
+	
+	private FeatureVector scaleFeatureVector(FeatureVector fv)
+	{
+		final double EPS = 0.00001;
+		FeatureVector newVector = new SimpleFeatureVector();
+
+		int featureIdx = 0;
+		for(String name: fv.getFeatureNames()) {
+			//scaling function: y = a*x+b
+			// 0 = a*v_min + b
+			// 1 = a*v_max + b
+			if(Math.abs(limits[featureIdx].getMax()-limits[featureIdx].getMin()) < EPS) {
+				newVector.addFeature(name, 1.0);
+			} else {
+				Double featureValue = fv.getFeature(name);
+				Double a = 1/(limits[featureIdx].getMax()-limits[featureIdx].getMin());
+				Double b = -a*limits[featureIdx].getMin();
+				
+				featureValue = a*featureValue+b; 
+				newVector.addFeature(name, featureValue);
+			}
+			++featureIdx;
+		}
+		return newVector;
+	}
+	
+	private Dataset buildDatasetForTraining(List<HMMTrainingElement<BxZoneLabel>> trainingElements)
+	{
+		Dataset data = new DefaultDataset();
+		for(HMMTrainingElement<BxZoneLabel> trainingElem : trainingElements) {
+			FeatureVector fv = scaleFeatureVector(trainingElem.getObservation());
+			System.out.println("Training label: " + trainingElem.getLabel());
+			Instance dataInstance = new DenseInstance(ArrayUtils.toPrimitive(fv.getFeatures()), trainingElem.getLabel());
+			data.add(dataInstance);
+		}
+		assert data.size() == trainingElements.size();
+		return data;
+	}
+	
+	private Dataset buildDatasetForClassification(List<FeatureVector> featureVectors)
+	{
+		Dataset data = new DefaultDataset();
+		for(FeatureVector fv: featureVectors) {
+			FeatureVector newVector = scaleFeatureVector(fv);
+			Instance dataInstance = new DenseInstance(ArrayUtils.toPrimitive(newVector.getFeatures()));
+			data.add(dataInstance);
+		}
+		assert data.size() == featureVectors.size();
+		return data;
+	}
+	
+	@Override
+	public void classifyZones(BxDocument document) throws AnalysisException 
+	{       
+		for (BxPage page : document.getPages()) {
+			for (BxZone zone : page.getZones()) {
+				List<FeatureVector> featureVectors = new ArrayList<FeatureVector>();
+				featureVectors.add(featureVectorBuilder.getFeatureVector(zone, page));
+				
+				Dataset unclassifiedData = buildDatasetForClassification(featureVectors);
+
+				Instance instance = unclassifiedData.get(0);
+				Object o = svm.classify(instance);
+				BxZoneLabel predictedClassValue = (BxZoneLabel)o; 
+
+				zone.setLabel(predictedClassValue);
+			}
+		}
+	} 
+
+	public double[] getWeights() {
+		return svm.getWeights();
+	}
+
+	private static class FeatureLimits 
+	{
+		public FeatureLimits(Double minValue, Double maxValue) {
+			min = minValue;
+			max = maxValue;
+		}
+		public double getMin() {
+			return min;
+		}
+		public void setMin(double min) {
+			this.min = min;
+		}
+		public double getMax() {
+			return max;
+		}
+		public void setMax(double max) {
+			this.max = max;
+		}
+		private double min;
+		private double max;
+	}
+}
---
+++ yadda-analysis-impl/src/main/java/pl/edu/icm/yadda/analysis/classification/svm/SVMZoneClassifierDemo.java
@@ -0,0 +1,207 @@
+package pl.edu.icm.yadda.analysis.classification.svm;
+
+import com.thoughtworks.xstream.XStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import pl.edu.icm.yadda.analysis.AnalysisException;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureCalculator;
+import pl.edu.icm.yadda.analysis.classification.features.FeatureVectorBuilder;
+import pl.edu.icm.yadda.analysis.classification.features.SimpleFeatureVectorBuilder;
+import pl.edu.icm.yadda.analysis.classification.hmm.HMMServiceImpl;
+import pl.edu.icm.yadda.analysis.classification.hmm.probability.HMMProbabilityInfo;
+import pl.edu.icm.yadda.analysis.classification.hmm.probability.HMMProbabilityInfoFactory;
+import pl.edu.icm.yadda.analysis.classification.hmm.tools.DocumentsExtractor;
+import pl.edu.icm.yadda.analysis.classification.hmm.tools.ZipExtractor;
+import pl.edu.icm.yadda.analysis.classification.hmm.training.HMMTrainingElement;
+import pl.edu.icm.yadda.analysis.metadata.zoneclassification.features.*;
+import pl.edu.icm.yadda.analysis.metadata.zoneclassification.nodes.BxDocsToFVHMMTrainingElementsConverterNode;
+import pl.edu.icm.yadda.analysis.textr.HMMZoneClassifier;
+import pl.edu.icm.yadda.analysis.textr.HierarchicalReadingOrderResolver;
+import pl.edu.icm.yadda.analysis.textr.ReadingOrderResolver;
+import pl.edu.icm.yadda.analysis.textr.ZoneClassifier;
+import pl.edu.icm.yadda.analysis.textr.model.*;
+import pl.edu.icm.yadda.analysis.textr.tools.DocumentPreprocessor;
+import pl.edu.icm.yadda.analysis.textr.tools.InitiallyClassifiedZonesPreprocessor;
+import pl.edu.icm.yadda.analysis.textr.transformers.TrueVizToBxDocumentReader;
+import pl.edu.icm.yadda.metadata.transformers.TransformationException;
+
+/**
+ *
+ * @author Pawel Szostek (p.szostek@icm.edu.pl)
+ */
+
+public class SVMZoneClassifierDemo {
+
+	protected static final String hmmTrainingFile = "/pl/edu/icm/yadda/analysis/metadata/zoneclassification/xmls.zip";
+	private static final String hmmTestFile = "/pl/edu/icm/yadda/analysis//metadata/zoneclassification/09629351.xml";
+    
+	public static BxDocument getTestFile() throws TransformationException, AnalysisException {
+        InputStream is = SVMZoneClassifierDemo.class.getResourceAsStream(hmmTestFile);
+        InputStreamReader isr = new InputStreamReader(is);
+        
+        TrueVizToBxDocumentReader reader = new TrueVizToBxDocumentReader();
+        List<BxPage> pages = reader.read(isr);
+        BxDocument testDocument = new BxDocument().setPages(pages);
+        return testDocument;
+	}
+  
+    public static void main(String[] args) throws TransformationException, Exception {
+        
+        // 1.1 construct vector of features builder
+    	List<FeatureCalculator<BxZone, BxPage>> featureCalculators = Arrays.<FeatureCalculator<BxZone, BxPage>>asList(
+        		new AbstractFeature(),
+        		new AcknowledgementFeature(),
+        		new AffiliationFeature(),
+                new AtCountFeature(),
+                new AtRelativeCountFeature(),
+                new AuthorFeature(),
+                new BibinfoFeature(),
+        		new BracketRelativeCount(),
+        		new BracketedLineRelativeCount(),
+                new CharCountFeature(),
+                new CharCountRelativeFeature(),
+                new CommaCountFeature(),
+                new CommaRelativeCountFeature(),
+        		new ContainsCuePhrasesFeature(),
+        		new CuePhrasesRelativeCountFeature(),
+        		new DatesFeature(),
+                new DigitCountFeature(),
+                new DigitRelativeCountFeature(),
+        		new DistanceFromNearestNeighbourFeature(),
+        		new DotCountFeature(),
+        		new DotRelativeCountFeature(),
+                new EmptySpaceRelativeFeature(),
+        		new FontHeightMeanFeature(),
+        		new FigureFeature(),
+        		new FreeSpaceWithinZoneFeature(),
+                new HeightFeature(),
+                new HeightRelativeFeature(),
+        		new HorizontalRelativeProminenceFeature(),
+        		new IsFirstPageFeature(),
+        		new IsFontBiggerThanNeighboursFeature(),
+        		new IsHighestOnThePageFeature(),
+        		new IsLastPageFeature(),
+        		new IsLowestOnThePageFeature(),
+        		new IsItemizeFeature(),
+                new KeywordsFeature(),
+                new LineCountFeature(),
+                new LineRelativeCountFeature(),
+                new LineHeightMeanFeature(),
+                new LineWidthMeanFeature(),
+                new LineXPositionMeanFeature(),
+                new LineXPositionDiffFeature(),
+                new LineXWidthPositionDiffFeature(),
+                new LetterCountFeature(),
+                new LetterRelativeCountFeature(),
+                new LowercaseCountFeature(),
+                new LowercaseRelativeCountFeature(),
+                new ContainsPageNumberFeature(),
+                new ProportionsFeature(),
+                new PunctuationRelativeCountFeature(),
+                new ReferencesFeature(),
+                new ReferencesTitleFeature(),
+                new StartsWithDigitFeature(),
+                new UppercaseCountFeature(),
+                new UppercaseRelativeCountFeature(),
+                new UppercaseWordCountFeature(),
+                new UppercaseWordRelativeCountFeature(),
+        		new VerticalProminenceFeature(),
+                new WidthFeature(),
+                new WordCountFeature(),
+                new WordCountRelativeFeature(),
+        		new WordWidthMeanFeature(),
+        		new WordLengthMeanFeature(),
+        		new WordLengthMedianFeature(),
+        		new WhitespaceCountFeature(),
+        		new WhitespaceRelativeCountLogFeature(),
+                new WidthRelativeFeature(),
+                new XPositionFeature(),
+                new XPositionRelativeFeature(),
+                new YPositionFeature(),
+                new YPositionRelativeFeature(),
+                new YearFeature()
+                );
+        FeatureVectorBuilder<BxZone, BxPage> vectorBuilder =
+                new SimpleFeatureVectorBuilder<BxZone, BxPage>();
+        vectorBuilder.setFeatureCalculators(featureCalculators);
+
+        /* import training documents */
+        DocumentsExtractor extractor = new ZipExtractor(hmmTrainingFile);
+        List<BxDocument> trainingList = extractor.getDocuments();
+        
+        /* open test file */
+        //BxDocument testDocument = getTestFile();
+        //Random randomGenerator = new Random();
+        int testDocIdx = 41;//randomGenerator.nextInt(trainingList.size());
+        BxDocument testDocument = trainingList.get(testDocIdx);
+
+        /* generate training set based on sequences and vector of features */
+        BxDocsToFVHMMTrainingElementsConverterNode node = new BxDocsToFVHMMTrainingElementsConverterNode();
+        node.setFeatureVectorBuilder(vectorBuilder);
+        node.setLabelMap(BxZoneLabel.getLabelToGeneralMap());
+        List<HMMTrainingElement<BxZoneLabel>> trainingElementsUnrevised = node.process(trainingList, null);
+        
+        Map<BxZoneLabel, Integer> labelCount = new HashMap<BxZoneLabel, Integer>();
+        labelCount.put(BxZoneLabel.GEN_BODY, 0);
+        labelCount.put(BxZoneLabel.GEN_METADATA, 0);
+        labelCount.put(BxZoneLabel.GEN_OTHER, 0);
+        labelCount.put(BxZoneLabel.GEN_REFERENCES, 0);
+        
+        for(HMMTrainingElement<BxZoneLabel> elem: trainingElementsUnrevised) {
+        	labelCount.put(elem.getLabel(), labelCount.get(elem.getLabel())+1);
+        }
+        
+        Integer max = Integer.MAX_VALUE;
+        for(BxZoneLabel lab: labelCount.keySet()) {
+        	if(labelCount.get(lab) < max)
+        		max = labelCount.get(lab);
+        	System.out.println(lab + " " + labelCount.get(lab));
+        }
+        
+        labelCount.put(BxZoneLabel.GEN_BODY, 0);
+        labelCount.put(BxZoneLabel.GEN_METADATA, 0);
+        labelCount.put(BxZoneLabel.GEN_OTHER, 0);
+        labelCount.put(BxZoneLabel.GEN_REFERENCES, 0);
+        List<HMMTrainingElement<BxZoneLabel>> trainingElements = new ArrayList<HMMTrainingElement<BxZoneLabel>>();
+        
+        for(HMMTrainingElement<BxZoneLabel> elem: trainingElementsUnrevised) {
+        	if(labelCount.get(elem.getLabel()) < max*1.3) {
+        		trainingElements.add(elem);
+        		labelCount.put(elem.getLabel(), labelCount.get(elem.getLabel())+1);
+        	}
+        }
+        System.out.println(max);
+        System.out.println(trainingElements.size());
+
+        List<BxDocument> testList = new ArrayList<BxDocument>(1);
+        testList.add(testDocument);
+        List<HMMTrainingElement<BxZoneLabel>> testElement = node.process(testList, null);
+
+        /* build a classifier */
+        SVMZoneClassifier zoneClassifier = new SVMZoneClassifier(BxZoneLabel.valuesOfCategory(BxZoneLabelCategory.CAT_GENERAL), vectorBuilder);
+        zoneClassifier.buildClassifier(trainingElements);
+        
+        /* classify zones from the test file */
+        zoneClassifier.classifyZones(testDocument);
+        assert testDocument.asZones().size() == testList.size();
+        for(int zoneIdx=0; zoneIdx < testDocument.asZones().size(); ++zoneIdx) {
+        	System.out.println("Recognized label: " + testDocument.asZones().get(zoneIdx).getLabel() + "[" + testElement.get(zoneIdx).getLabel() + "]");
+        	System.out.println(testDocument.asZones().get(zoneIdx).toText());
+        }
+    }
+}

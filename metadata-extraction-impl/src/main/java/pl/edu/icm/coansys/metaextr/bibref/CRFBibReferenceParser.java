ecf7384c1402fb6d2304e9a53d4e389ca35c87f7
Dominika Tkaczyk <d.tkaczyk@icm.edu.pl>
2012-10-05 17:23:01 +0200

    different way of reading model

--- metadata-extraction-impl/src/main/java/pl/edu/icm/coansys/metaextr/bibref/CRFBibReferenceParser.java
+++ metadata-extraction-impl/src/main/java/pl/edu/icm/coansys/metaextr/bibref/CRFBibReferenceParser.java
@@ -1,6 +1,5 @@
 package pl.edu.icm.coansys.metaextr.bibref;
 
-import pl.edu.icm.coansys.metaextr.bibref.BibReferenceParser;
 import edu.umass.cs.mallet.base.pipe.Pipe;
 import edu.umass.cs.mallet.base.pipe.iterator.LineGroupIterator;
 import edu.umass.cs.mallet.base.types.InstanceList;
@@ -10,11 +9,11 @@ import java.io.*;
 import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 import org.apache.commons.lang.StringUtils;
-import pl.edu.icm.coansys.metaextr.exception.AnalysisException;
 import pl.edu.icm.coansys.metaextr.bibref.model.BibEntry;
 import pl.edu.icm.coansys.metaextr.bibref.parsing.model.Citation;
 import pl.edu.icm.coansys.metaextr.bibref.parsing.model.CitationTokenLabel;
 import pl.edu.icm.coansys.metaextr.bibref.parsing.tools.CitationUtils;
+import pl.edu.icm.coansys.metaextr.exception.AnalysisException;
 
 /**
  *
@@ -22,41 +21,48 @@ import pl.edu.icm.coansys.metaextr.bibref.parsing.tools.CitationUtils;
  */
 public class CRFBibReferenceParser implements BibReferenceParser<BibEntry> {
     
-    private String modelFile = "/tmp/acrf.ser.gz";
+    private ACRF model;
 
     @Override
 	public BibEntry parseBibReference(String text) throws AnalysisException {
-        try {
-            Citation citation = CitationUtils.stringToCitation(text);
-            String data = StringUtils.join(CitationUtils.citationToMalletInputFormat(citation), "\n");
-
-            ACRF model = readModel();
-            Pipe pipe = model.getInputPipe();
-            InstanceList instanceList = new InstanceList(pipe);
-            instanceList.add(new LineGroupIterator(new StringReader(data), Pattern.compile ("\\s*"), true)); 
-            LabelsSequence labelSequence = (LabelsSequence)model.getBestLabels(instanceList).get(0);
-            
-            for (int i = 0; i < labelSequence.size(); i++) {
-                citation.getTokens().get(i).setLabel(CitationTokenLabel.valueOf(labelSequence.get(i).toString()));
-            }
-            
-            return CitationUtils.citationToBibref(citation);
-		
-        } catch (ClassNotFoundException ex) {
-            throw new AnalysisException("Cannot parse reference", ex);
-        } catch (IOException ex) {
-            throw new AnalysisException("Cannot parse reference", ex);
+        if (model == null) {
+            throw new AnalysisException("Model object is not set!");
         }
-	}
-    
-    private ACRF readModel() throws IOException, ClassNotFoundException {
-        InputStream is = new FileInputStream(new File(modelFile));
-        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new GZIPInputStream(is)));
-        return (ACRF)(ois.readObject());
-    }
+        
+        Citation citation = CitationUtils.stringToCitation(text);
+        String data = StringUtils.join(CitationUtils.citationToMalletInputFormat(citation), "\n");
 
-    public void setModelFile(String modelFile) {
-        this.modelFile = modelFile;
+        Pipe pipe = model.getInputPipe();
+        InstanceList instanceList = new InstanceList(pipe);
+        instanceList.add(new LineGroupIterator(new StringReader(data), Pattern.compile ("\\s*"), true)); 
+        LabelsSequence labelSequence = (LabelsSequence)model.getBestLabels(instanceList).get(0);
+           
+        for (int i = 0; i < labelSequence.size(); i++) {
+            citation.getTokens().get(i).setLabel(CitationTokenLabel.valueOf(labelSequence.get(i).toString()));
+        }
+            
+        return CitationUtils.citationToBibref(citation);
+    }
+    
+    public void setModel(String modelFile) throws AnalysisException {
+        InputStream is;
+        try {
+            is = new FileInputStream(new File(modelFile));
+        } catch (FileNotFoundException ex) {
+            throw new AnalysisException("Cannot set model!", ex);
+        }
+        setModel(is);
+    }
+    
+    public void setModel(InputStream modelInputStream) throws AnalysisException {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new GZIPInputStream(modelInputStream)));
+            model = (ACRF)(ois.readObject());
+        } catch (IOException ex) {
+            throw new AnalysisException("Cannot set model!", ex);
+        } catch (ClassNotFoundException ex) {
+            throw new AnalysisException("Cannot set model!", ex);
+        }
     }
     
 }

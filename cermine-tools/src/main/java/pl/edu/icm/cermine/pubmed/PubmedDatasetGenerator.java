896c9d7d894e59084a080833fdd68a43e1bb929b
Pawel Szostek <p.szostek@icm.edu.pl>
2012-10-29 16:40:36 +0100

    add token-based levenshtein metric

--- cermine-tools/src/main/java/pl/edu/icm/cermine/pubmed/PubmedDatasetGenerator.java
+++ cermine-tools/src/main/java/pl/edu/icm/cermine/pubmed/PubmedDatasetGenerator.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -25,8 +26,138 @@ import org.xml.sax.SAXException;
 import pl.edu.icm.cermine.PdfBxStructureExtractor;
 import pl.edu.icm.cermine.exception.AnalysisException;
 import pl.edu.icm.cermine.structure.model.BxDocument;
+import pl.edu.icm.cermine.structure.model.BxZone;
 
 public class PubmedDatasetGenerator {
+	private static class Levenshtein {
+		public Integer compare(List<String> s1, List<String> s2)
+	    {
+	        int retval = 0;
+	        final int n = s1.size();
+	        final int m = s2.size();
+	        if (n == 0) {
+	            retval = m;
+	        } else if (m == 0) {
+	            retval = n;
+	        } else {
+	            retval = compare(s1, n, s2, m);
+	        }
+	        return retval;
+	    }
+
+		private int compare(List<String> s1, final int n, List<String> s2, final int m) {
+			int matrix[][] = new int[n + 1][m + 1];
+			for (int i = 0; i <= n; i++) {
+				matrix[i][0] = i;
+			}
+			for (int i = 0; i <= m; i++) {
+				matrix[0][i] = i;
+			}
+
+			for (int i = 1; i <= n; i++) {
+				String s1i = s1.get(i - 1);
+				for (int j = 1; j <= m; j++) {
+					String s2j = s2.get(j - 1);
+					final int cost = s1i.equals(s2j) ? 0 : 1;
+					matrix[i][j] = min3(matrix[i - 1][j] + 1,
+							matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost);
+				}
+			}
+			return matrix[n][m];
+		}
+
+		private int min3(final int a, final int b, final int c) {
+			return Math.min(Math.min(a, b), c);
+		}
+	}
+
+	private static <T> List<T> slice(List<T> list, int index, int count) {
+		List<T> result = new ArrayList<T>();
+		if (index >= 0 && index < list.size()) {
+			int end = index + count < list.size() ? index + count : list.size();
+			for (int i = index; i < end; i++) {
+				result.add(list.get(i));
+			}
+		}
+		return result;
+	}
+	
+	private static Integer min3(Integer int1, Integer int2, Integer int3) {
+		return Math.min(int1, Math.min(int2, int3));
+	}
+	
+	private static List<String> tokenize(String text) {
+		List<String> roughRet = new ArrayList<String>(Arrays.asList(text.split(" |,|\\.|&|;|:|\\-")));
+		List<String> ret = new ArrayList<String>();
+		for(String candidate: roughRet) {
+			if(!candidate.isEmpty()) {
+				ret.add(candidate);
+			}
+		}
+		return ret;
+	}
+
+	private static String joinStrings(List<String> strings) {
+		StringBuilder ret = new StringBuilder();
+		for(String str: strings) {
+			ret.append(str).append(" ");
+		}
+		return ret.toString();
+	}
+	
+	private static Integer levenshteinDistance(List<String> tokens1, List<String> tokens2) {
+		Integer length1 = tokens1.size();
+		Integer length2 = tokens2.size();
+		Integer distance = 0;
+		if(length1 == 0) {
+			return length2;
+		}
+		else if(length2 == 0) {
+			return length1;
+		}
+		else {
+			if(!tokens1.get(0).equals(tokens2.get(0))) {
+				distance = 1;
+			}
+			return min3(	levenshteinDistance(slice(tokens1, 1, length1-1), tokens2) + 1,
+							levenshteinDistance(tokens1, slice(tokens2, 1, length2-1)) + 1,
+							levenshteinDistance(slice(tokens1, 1, length1-1), slice(tokens2, 1, length2-1)) + distance
+						);
+		}
+	}
+
+	private static List<String> extractTextAsList(Node node) {
+		List<String> ret = new ArrayList<String>();
+		
+		if(node == null) {
+			return ret;
+		}
+		if(node.getChildNodes().getLength() == 0) {
+			ret.add(node.getNodeValue());
+		} else {
+			for(Integer childIdx=0; childIdx < node.getChildNodes().getLength(); ++childIdx) {
+				ret.addAll(extractTextAsList(node.getChildNodes().item(childIdx)));
+			}
+		}
+		return ret;
+	}
+
+	private static String extractTextFromNodes(Node node) {
+		StringBuilder ret = new StringBuilder();
+		
+		if(node == null) {
+			return "";
+		}
+		if(node.getChildNodes().getLength() == 0) {
+			return node.getNodeValue() + " ";
+		} else {
+			for(Integer childIdx=0; childIdx < node.getChildNodes().getLength(); ++childIdx) {
+				ret.append(extractTextFromNodes(node.getChildNodes().item(childIdx)));
+			}
+		}
+		return ret.toString();
+	}
+
 	public static void main(String[] args) throws AnalysisException, ParserConfigurationException, SAXException, IOException, XPathExpressionException {
 		if(args.length != 2) {
 			System.err.println("Usage: <pubmed .nxml path> <corresponding .pdf path>");
@@ -54,7 +185,8 @@ public class PubmedDatasetGenerator {
 		BxDocument bxDoc = structureExtractor.extractStructure(pdfStream);
 		
 		//abstract
-		String abstractString = (String) xpath.evaluate("/article/front/article-meta/abstract", domDoc, XPathConstants.STRING);
+		Node abstractNode = (Node) xpath.evaluate("/article/front/article-meta/abstract", domDoc, XPathConstants.NODE);
+		String abstractString = extractTextFromNodes(abstractNode);
 		//title
 		String titleString = (String) xpath.evaluate("/article/front/article-meta/title-group/article-title", domDoc, XPathConstants.STRING);
 		
@@ -81,13 +213,16 @@ public class PubmedDatasetGenerator {
 		//publication date
 		String pubdateString = null;
 		if(((NodeList)xpath.evaluate("/article/front/article-meta/pub-date", domDoc, XPathConstants.NODESET)).getLength() > 1) {
-			pubdateString = (String) xpath.evaluate("/article/front/article-meta/pub-date[@pub-type='epub']", domDoc, XPathConstants.STRING);
+			Node pubdateNode = (Node) xpath.evaluate("/article/front/article-meta/pub-date[@pub-type='epub']", domDoc, XPathConstants.NODE);
+			pubdateString = extractTextFromNodes(pubdateNode);
 		} else {
-			pubdateString = (String) xpath.evaluate("/article/front/article-meta/pub-date[@pub-type='collection']", domDoc, XPathConstants.STRING);
+			Node pubdateNode = (Node) xpath.evaluate("/article/front/article-meta/pub-date[@pub-type='collection']", domDoc, XPathConstants.NODE);
+			pubdateString = extractTextFromNodes(pubdateNode);
 		}
 		
 		//keywords
-		String keywordsString = (String) xpath.evaluate("/article/front/article-meta/kwd-group/kwd", domDoc, XPathConstants.STRING);
+		Node keywordsNode = (Node) xpath.evaluate("/article/front/article-meta/kwd-group", domDoc, XPathConstants.NODE);
+		String keywordsString = extractTextFromNodes(keywordsNode);
 		//DOI
 		String doiString = (String) xpath.evaluate("/article/front/article-meta/article-id[@pub-id-type='doi']", domDoc, XPathConstants.STRING);
 		//volume
@@ -130,7 +265,7 @@ public class PubmedDatasetGenerator {
 				try {
 					email  = (String)xpath.evaluate("email", curNode, XPathConstants.STRING);
 				} catch (XPathExpressionException e) {
-					//do nothing
+					//yaaay, probably there is no e-mail at all! => do nothing
 				}
 			}
 			authorEmails.add(email);
@@ -146,9 +281,21 @@ public class PubmedDatasetGenerator {
 		}
 		
 		//article body
-		String bodyString = (String)xpath.evaluate("/article/body", domDoc, XPathConstants.STRING);
+		System.out.println("body");
+		Node bodyNode = (Node) xpath.evaluate("/article/body", domDoc, XPathConstants.NODE);
+		List<String> bodyStrings = extractTextAsList(bodyNode);
+//				new ArrayList<String>();
+//		for(Integer bodyNodeIdx = 0; bodyNodeIdx < bodyNode.getChildNodes().getLength(); ++bodyNodeIdx) {
+//			bodyStrings.add(extractTextFromNodes(bodyNode.getChildNodes().item(bodyNodeIdx)));
+//		}
 		
-		String refString = (String)xpath.evaluate("/article/back/ref-list", domDoc, XPathConstants.STRING);
+		//references
+		NodeList refParentNode = (NodeList) xpath.evaluate("/article/back/ref-list", domDoc, XPathConstants.NODESET);
+		List<String> refStrings = new ArrayList<String>();
+		for(Integer refIdx = 0; refIdx < refParentNode.getLength(); ++refIdx) {
+			refStrings.add(extractTextFromNodes(refParentNode.item(refIdx)));
+		}
+		
 		
 		System.out.println("journalTitle: " + journalTitleString);
 		System.out.println("journalPublisher: " + journalPublisherString);
@@ -172,9 +319,9 @@ public class PubmedDatasetGenerator {
 		System.out.println("firstPage: " + firstPageString);
 		System.out.println("lastPage: " + lastPageString);
 		
-		System.out.println("body: " + bodyString);
+		System.out.println("body: " + bodyStrings);
 		
-		System.out.println("ref: " + refString);
+		System.out.println("ref: " + refStrings.size() + " " + refStrings);
 		/*
 		#MET_TITLE
 		/article/front/article-meta/title-group/article-title 
@@ -244,7 +391,19 @@ public class PubmedDatasetGenerator {
 		/article/body
 		 
 		 */
-		
-		
+	Levenshtein lev = new Levenshtein();
+	Double max = 0.0;
+	BxZone bestZone = null;
+	for(BxZone zone: bxDoc.asZones()) {
+		Integer sim = lev.compare(tokenize(abstractString), tokenize(zone.toText()));
+//		if(sim > max) {
+//			bestZone = zone;
+//			max = sim;
+//		}
+//		if(sim > 0.2) {
+			System.out.println(sim + zone.toText() + "\n");
+//		}
+	}
+	
 	}
 }

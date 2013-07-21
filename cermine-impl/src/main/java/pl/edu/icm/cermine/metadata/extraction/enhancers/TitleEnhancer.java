752da18c7961dfdc62056fb38c05a1b1ebeabc59
Dominika Tkaczyk <d.tkaczyk@icm.edu.pl>
2013-07-21 02:04:43 +0200

    title enhancer updated

--- cermine-impl/src/main/java/pl/edu/icm/cermine/metadata/extraction/enhancers/TitleEnhancer.java
+++ cermine-impl/src/main/java/pl/edu/icm/cermine/metadata/extraction/enhancers/TitleEnhancer.java
@@ -24,12 +24,6 @@ public class TitleEnhancer extends AbstractSimpleEnhancer {
     }
 
     @Override
-    protected boolean enhanceMetadata(BxZone zone, Element metadata) {
-        Enhancers.setTitle(metadata, zone.toText().replaceAll("\n", " "));
-        return true;
-    }
-
-    @Override
     protected boolean enhanceMetadata(BxPage page, Element metadata) {
         List<BxZone> titleZones = new ArrayList<BxZone>();
         for (BxZone zone : filterZones(page)) {
@@ -45,8 +39,26 @@ public class TitleEnhancer extends AbstractSimpleEnhancer {
 
         });
 
-        for (BxZone zone : titleZones) {
-            if (enhanceMetadata(zone, metadata)) {
+        if (!titleZones.isEmpty()) {
+            BxZone titleZone = titleZones.get(0);
+            double height = titleZone.getLines().get(0).getHeight();
+            while (titleZone.hasPrev() 
+                    && BxZoneLabel.MET_TITLE.equals(titleZone.getPrev().getLabel())
+                    && Math.abs(height-titleZone.getPrev().getLines().get(0).getHeight()) < 0.5) {
+                titleZone = titleZone.getPrev();
+            }
+            
+            String title = titleZone.toText();
+            while (titleZone.hasNext() 
+                    && BxZoneLabel.MET_TITLE.equals(titleZone.getNext().getLabel())
+                    && Math.abs(height-titleZone.getNext().getLines().get(0).getHeight()) < 0.5) {
+                titleZone = titleZone.getNext();
+                title += " ";
+                title += titleZone.toText();
+            }
+
+            if (!title.isEmpty()) {
+                Enhancers.setTitle(metadata, title.trim().replaceAll("\n", " "));
                 return true;
             }
         }

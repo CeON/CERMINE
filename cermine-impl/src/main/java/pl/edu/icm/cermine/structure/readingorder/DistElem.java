2d23ebd4aae7bf391f6308fe8df9d7a8a2dc95fd
Dominika Tkaczyk <d.tkaczyk@icm.edu.pl>
2014-06-21 01:51:15 +0200

    s/Double/double/

--- cermine-impl/src/main/java/pl/edu/icm/cermine/structure/readingorder/DistElem.java
+++ cermine-impl/src/main/java/pl/edu/icm/cermine/structure/readingorder/DistElem.java
@@ -129,7 +129,7 @@ public class DistElem<E> implements Comparable<DistElem<E> >{
 	}
 	
 	private int compareDouble(double d1, double d2) {
-		double eps = new Double(1E-6);
+		double eps = 1E-6;
 		if(d1 > d2) {
 			return 1;
 		} else if(Math.abs(d1-d2) < eps) {

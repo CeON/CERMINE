598faa69fbb80d32f4ec3ba86192d2c8c9abfb33
Mateusz Fedoryszak <matfed@icm.edu.pl>
2015-04-28 12:11:32 +0200

    Small SimpleCORSFilter fixes

--- cermine-web/src/main/java/pl/edu/icm/cermine/web/controller/SimpleCORSFilter.java
+++ cermine-web/src/main/java/pl/edu/icm/cermine/web/controller/SimpleCORSFilter.java
@@ -25,6 +25,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
@@ -35,16 +36,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class SimpleCORSFilter implements Filter {
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-		chain.doFilter(req, res);
-	}
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (((HttpServletRequest) req).getHeader("Origin") != null) {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        }
+        chain.doFilter(req, res);
+    }
 
-	public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
-	public void destroy() {}
+    public void destroy() {
+    }
 }

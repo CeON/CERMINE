254cf3ec7ec121eadb4abb14017023faf643c66a
Dominika Tkaczyk <d.tkaczyk@icm.edu.pl>
2018-01-30 11:31:12 +0000

    change date in copyright

--- cermine-impl/src/main/java/pl/edu/icm/cermine/metadata/extraction/enhancers/AuthorAffiliationGeometricEnhancer.java
+++ cermine-impl/src/main/java/pl/edu/icm/cermine/metadata/extraction/enhancers/AuthorAffiliationGeometricEnhancer.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of CERMINE project.
- * Copyright (c) 2011-2016 ICM-UW
+ * Copyright (c) 2011-2018 ICM-UW
  *
  * CERMINE is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
